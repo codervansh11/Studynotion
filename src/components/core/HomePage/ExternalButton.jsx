2f33d0f9b5fe2b3fe086e0c826449d3662e7e126
codervansh11 <vanshsabharwal100@gmail.com>
2025-02-28 18:51:04 +0530

    .

--- src/components/core/HomePage/ExternalButton.jsx
+++ src/components/core/HomePage/ExternalButton.jsx
@@ -5,6 +5,7 @@ const ExternalButton = ({ children, active, linkto }) => {
     <a
       href={linkto}
       target="_blank"
+      rel="noopener noreferrer"
     >
       <div
         className={`text-center text-[13px] sm:text-[16px] px-6 py-3 rounded-md font-bold shadow-[2px_2px_0px_0px_rgba(255,255,255,0.18)] ${
