41d70e7968401950d64352470cb40430aa0368a8
codervansh11 <vanshsabharwal100@gmail.com>
2025-05-11 20:31:16 +0530

    logic changed

--- src/components/core/Dashboard/AddCourse/Upload.jsx
+++ src/components/core/Dashboard/AddCourse/Upload.jsx
@@ -39,6 +39,7 @@ export default function Upload({
   })
 
   const previewFile = (file) => {
+    // console.log(file)
     const reader = new FileReader()
     reader.readAsDataURL(file)
     reader.onloadend = () => {
@@ -47,26 +48,25 @@ export default function Upload({
   }
 
   useEffect(() => {
-    register(name, { required: true })
-  }, [register, name])
+    register(name)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [register])
 
   useEffect(() => {
     setValue(name, selectedFile)
-  }, [selectedFile, setValue, name])
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedFile, setValue])
 
   return (
     <div className="flex flex-col space-y-2">
       <label className="text-sm text-richblack-5" htmlFor={name}>
         {label} {!viewData && <sup className="text-pink-200">*</sup>}
       </label>
-
       <div
         className={`${
           isDragActive ? "bg-richblack-600" : "bg-richblack-700"
         } flex min-h-[250px] cursor-pointer items-center justify-center rounded-md border-2 border-dotted border-richblack-500`}
-        {...getRootProps()}
       >
-        <input {...getInputProps()} ref={inputRef} />
         {previewSource ? (
           <div className="flex w-full flex-col p-6">
             {!video ? (
@@ -93,36 +93,27 @@ export default function Upload({
             )}
           </div>
         ) : (
-          <div className="flex w-full flex-col items-center p-6">
+          <div
+            className="flex w-full flex-col items-center p-6"
+            {...getRootProps()}
+          >
+            <input {...getInputProps()} ref={inputRef} />
             <div className="grid aspect-square w-14 place-items-center rounded-full bg-pure-greys-800">
               <FiUploadCloud className="text-2xl text-yellow-50" />
             </div>
             <p className="mt-2 max-w-[200px] text-center text-sm text-richblack-200">
               Drag and drop an {!video ? "image" : "video"}, or click to{" "}
-              <span
-                className="font-semibold text-yellow-50 cursor-pointer"
-                onClick={(e) => {
-                  e.stopPropagation()
-                  inputRef.current?.click()
-                }}
-              >
-                Browse
-              </span>{" "}
-              a file
+              <span className="font-semibold text-yellow-50">Browse</span> a
+              file
             </p>
-            <ul className="mt-10 flex list-disc justify-between space-x-12 text-center text-xs text-richblack-200">
+            <ul className="mt-10 flex list-disc justify-between space-x-12 text-center  text-xs text-richblack-200">
               <li>Aspect ratio 16:9</li>
               <li>Recommended size 1024x576</li>
             </ul>
           </div>
         )}
       </div>
-
-      {errors[name] && (
-        <span className="ml-2 text-xs tracking-wide text-pink-200">
-          {label} is required
-        </span>
-      )}
+      
     </div>
   )
 }
